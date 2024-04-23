869eac22699b8a8d2cf2eb149185caba5e65f923
HorvathMate04 <culubpenguin@gmail.com>
2024-04-23 22:10:48 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+function checkAnswers() {
+    const answers = {
+        question1: document.getElementById('question1').value.trim().toLowerCase(),
+        question2: document.getElementById('question2').value.trim().toLowerCase(),
+        question3: document.getElementById('question3').value.trim().toLowerCase()
+    };
+
+    const correctAnswers = {
+        question1: 'plebejusok',
+        question2: 'constantinápoly',
+        question3: 'lübeck'
+    };
+
+    let correctCount = 0;
+
+    if (answers.question1 === correctAnswers.question1) {
+        correctCount++;
+    }
+    if (answers.question2 === correctAnswers.question2) {
+        correctCount++;
+    }
+    if (answers.question3 === correctAnswers.question3) {
+        correctCount++;
+    }
+
+    const resultElement = document.getElementById('result');
+    resultElement.textContent = `Helyes válaszok száma: ${correctCount} / 3`;
+}
